562ae7a19f3ce09f91f3305afd656eb7bca3d20d
angel <ajjr0827@gmail.com>
2023-03-26 18:54:46 -0700

    Add project files.

---
+++ MVCB5/App_Start/BundleConfig.cs
@@ -0,0 +1,50 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MVCB5
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new Bundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/modernizr-*",
+                        "~/Scripts/bootstrap.min.js",
+                        "~/Scripts/toastr.min.js",
+                        "~/Content/datatable/datatables.min.js",
+                        "~/Content/datatable/Buttons-2.3.3/js/dataTables.buttons.min.js",
+                        "~/Content/datatable/Buttons-2.3.3/js/buttons.flash.min.js",
+                        "~/Content/datatable/JSZip-2.5.0/jszip.min.js",
+                        "~/Content/datatable/pdfmake-0.1.36/pdfmake.min.js",
+                        "~/Content/datatable/pdfmake-0.1.36/vfs_fonts.js",
+                        "~/Content/datatable/Buttons-2.3.3/js/buttons.html5.min.js",
+                        "~/Content/datatable/Buttons-2.3.3/js/buttons.print.min.js"
+                        ));
+
+            bundles.Add(new Bundle("~/bundles/app").Include(
+                    "~/Scripts/layoutJs/app.js"
+            ));
+
+            bundles.Add(new Bundle("~/bundles/Utils").Include(
+                    "~/Scripts/Utils/Utils.js"
+            ));
+
+
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/datatable/Buttons-2.3.3/css/datatables.min.css",
+                      "~/Content/datatable/buttons.dataTables.min.css",
+                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/toastr.min.css",
+                      "~/Content/fontawesome/css/all.css",
+                      "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/layoutStyles/css").Include(
+                      "~/Content/layoutStyles/app.css"));
+        }
+    }
+}
---
+++ MVCB5/Scripts/Utils/Utils.js
@@ -0,0 +1,22 @@
+﻿document.addEventListener("DOMContentLoaded", function (event) {
+    console.log('Holas')
+});
+function ajaxPost(url, data, successCallback, errorCallback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (successCallback) {
+                    successCallback(xhr.responseText);
+                }
+            } else {
+                if (errorCallback) {
+                    errorCallback(xhr.status, xhr.statusText);
+                }
+            }
+        }
+    };
+    xhr.send(JSON.stringify(data));
+}
